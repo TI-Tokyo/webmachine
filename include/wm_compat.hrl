c674f9d09a010a9db4ae821cb5739f973d931aa0
Andriy Zavada <johnhommer@gmail.com>
2024-05-06 21:42:53 +0300

    drop support for old get_stacktrace

--- include/wm_compat.hrl
+++ include/wm_compat.hrl
@@ -1,7 +1,2 @@
--ifndef(deprecate_stacktrace).
--define(STPATTERN(Pattern), Pattern).
--define(STACKTRACE, erlang:get_stacktrace()).
--else.
 -define(STPATTERN(Pattern), Pattern:__STACKTRACE).
 -define(STACKTRACE, __STACKTRACE).
--endif.
